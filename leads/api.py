8493204d0bd27ea32952dd953eba3933f02d91c6
panu2306 <pranavbhendawade@gmail.com>
2020-03-18 17:41:04 +0530

    api.py added

---
+++ leads/api.py
@@ -0,0 +1,11 @@
+from .models import Lead
+from .serializers import LeadSerializer
+from rest_framework import viewsets, permissions
+
+# Lead Viewset:
+class LeadViewSet(viewsets.ModelViewSet):
+    queryset = Lead.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = LeadSerializer
